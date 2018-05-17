37d47848fc41b8076d32dd91ec22289545b7b73d
Pat Luther <pluther@marinsoftware.com>
2018-05-16 21:56:37 -0700

    Initial working version

---
+++ djiConvert.py
@@ -0,0 +1,35 @@
+__author__ = 'pluther'
+
+import srtConvert
+import argparse
+import logging
+import os
+
+
+parser = argparse.ArgumentParser(description='Converts a .SRT file from a DJI drone to csv format')
+parser.add_argument('--debug', '-d', help='Runs script in debug mode', action='store_true', default=False)
+parser.add_argument('--inputFile', '--input', '-f', '-i', help='Location of original .SRT file', required=True)
+parser.add_argument('--outputFile', '--output', '-o', help='File to store the csv in')
+args = parser.parse_args()
+
+#Set logging level:
+if args.debug:
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    debug = True
+else:
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    debug = False
+
+inFile = args.inputFile
+
+if (args.outputFile):
+    logging.debug('Output file is given as ' + args.outputFile)
+    outFile = args.outputFile
+else:
+    outFile = os.path.splitext(inFile)[0] + '.csv'
+
+logging.info('Reading DJI Subtitle information from ' + inFile)
+logging.info('Output be written to ' + outFile)
+
+srtConvert.convertSRT(inFile,outFile, debug)
+
---
+++ srtConvert.py
@@ -0,0 +1,51 @@
+__author__ = 'pluther'
+
+import logging
+
+def setLoggingLevel(debug):
+    if debug:
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+def convertSRT(inFile,outFile,debug):
+    setLoggingLevel(debug)
+    logging.debug('Debug logging is on.')
+    csvFile = open(outFile,'w')
+
+    # First line of the csv file is the keys. Write then once before doing anything else
+    csvFile.write('Date,Time,Latitude,Longitude,Height\n')
+    with open(inFile) as f:
+        lines = f.read().splitlines()
+    for i in range(len(lines)):
+        if lines[i].startswith('HOME'):
+            lineparts = lines[i].split(' ')
+            date = lineparts[1]
+            time = lineparts[2]
+            logging.debug('Date: ' + str(date))
+            logging.debug('Time: ' + str(time))
+            if not (lines[i+1].startswith('GPS')):
+                logging.error('Line following HOME should be GPS. Instead, it is' )
+                logging.error(lines[i+1])
+                sys.exit(1)
+            else:
+                lineparts = lines[i+1].split(' ')
+
+                longlat = lineparts[0][4:-4]
+                ll = longlat.split(',')
+                longitude = ll[0]
+                latitude = ll[1]
+                logging.debug('Latitude: ' + str(latitude))
+                logging.debug('Longitude: ' + str(longitude))
+
+                height=lineparts[1][10:]
+                logging.debug('Height: ' + str(height))
+            # Now that we have all the information, write it to the file:
+            separator = ','
+            outputLine = separator.join([date,time,latitude,longitude,height])
+            csvFile.write(outputLine + '\n')
+
+
+
+
+
